368f2a4339e7a59c410636697e0dafe0acb2f92d
Buisiwe <buisiwemad@gmail.com>
2025-10-30 19:52:27 +0200

    created learning page, web automation tab and browser

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -3,10 +3,20 @@ package Pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 
 public class LoginPage {
     WebDriver driver;
-
+    @FindBy(id = "login-email")
+    WebElement loginEmail_id;
+    @FindBy(id = "login-password")
+    WebElement loginPassword_id;
+    @FindBy(id = "login-submit")
+    WebElement loginSubmit_id;
     @FindBy(id = "tab-btn-web")
     WebElement webAutomationAdvance_id;
 
@@ -14,7 +24,18 @@ public class LoginPage {
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
+    public void enterLoginEmail(String email) {
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(visibilityOf(loginEmail_id));
+        loginEmail_id.sendKeys(email);
+    }
 
+    public void enterPasswordId(String password) {
+        loginPassword_id.sendKeys(password);
+    }
+
+    public void clickLogin() {
+        loginSubmit_id.click();
+    }
     //method
     public void webAutomationAdvance() {
         webAutomationAdvance_id.click();
---
+++ src/test/java/TestData/Base.java
@@ -0,0 +1,19 @@
+package TestData;
+
+import Pages.HomePage;
+import Pages.LearningMaterialPage;
+import Pages.LoginPage;
+import Pages.WebAutomationAdvancePage;
+import Utils.BrowserFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class Base {
+    BrowserFactory browserFactory = new BrowserFactory();
+
+    final WebDriver driver = browserFactory.startBrowser("chrome", "https://www.ndosiautomation.co.za/");
+    HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+    LoginPage loginPage= PageFactory.initElements(driver, LoginPage.class);
+    LearningMaterialPage learningMaterialPage = PageFactory.initElements(driver, LearningMaterialPage.class);
+    WebAutomationAdvancePage webAutomationAdvancePage = PageFactory.initElements(driver, WebAutomationAdvancePage.class);
+}
